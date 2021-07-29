0bcc033f283a503802517c85fd4592638d27169e
Chao-ying Fu <cfu@wavecomp.com>
2021-07-29 09:43:23 -0700

    Support system reset for the Boston board.

--- arch/riscv/lib/reset.c
+++ arch/riscv/lib/reset.c
@@ -6,6 +6,12 @@
 #include <command.h>
 #include <cpu_func.h>
 
+void __weak _machine_restart(void)
+{
+	printf("reset not supported yet\n");
+	hang();
+}
+
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	reset_cpu();
