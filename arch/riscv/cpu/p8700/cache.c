ccc56b27eeca53cc154ae9076519b1c46d4e635e
Chao-ying Fu <cfu@mips.com>
2025-05-09 11:25:55 +0200

    riscv: p8700: Add Coherence Manager (CM) and IOCU support
    Add support for Coherence Manager (CM) and IOCU discovery and
    configuration on the P8700 platform.
    
    Signed-off-by: Chao-ying Fu <cfu@mips.com>
    Signed-off-by: Uros Stajic <uros.stajic@htecgroup.com>

--- arch/riscv/cpu/p8700/cache.c
+++ arch/riscv/cpu/p8700/cache.c
@@ -48,6 +48,9 @@ static void probe_cache_config(void)
 
 void flush_dcache_range(unsigned long start, unsigned long end)
 {
+	if (p8700_dma_is_coherent())
+		return;
+
 	if (lsize == 0)
 		probe_cache_config();
 
@@ -71,6 +74,9 @@ void flush_dcache_range(unsigned long start, unsigned long end)
 
 void invalidate_dcache_range(unsigned long start, unsigned long end)
 {
+	if (p8700_dma_is_coherent())
+		return;
+
 	if (lsize == 0)
 		probe_cache_config();
 
